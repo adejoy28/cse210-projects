a007b9eeaa1c42fbb3ce7d15c43a931fdaa07703
adejoy28 <adejoy28@gmail.com>
2024-11-01 11:36:20 +0100

    sandbosx updated

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -19,6 +19,29 @@ class Program
 
         // Console.WriteLine(color);
 
-       
+        // Random randomGenerator = new Random();
+        // int number = randomGenerator.Next(1, 11);
+
+        // Console.WriteLine(number);
+
+        // While loop
+        // int i = 1;
+        // while (i < 18)
+        // {
+        //     Console.WriteLine(i);
+        //     i += 6;
+        // }
+
+        // do
+        // {
+        //     Console.WriteLine("He is a fine boy " + i);
+        //     i++;
+        // } while (i < 20);
+
+        for (int percent = 0; percent < 100; percent += 10)
+        {
+            Console.WriteLine(percent);
+        }
+
     }
 }
