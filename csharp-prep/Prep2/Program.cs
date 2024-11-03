51230e182a708eb3021ca2cdd7aa0b12563f3005
adejoy28 <adejoy28@gmail.com>
2024-11-04 00:46:07 +0100

    update prep4 assign

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,80 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Console.WriteLine("Hello Prep2 World!");
+
+        // Writr a program that determines the letter grade for a course according to the following scal
+
+        // As the user for their score
+        Console.Write("What is your grade? ");
+        string userScore = Console.ReadLine();
+        int userScoreInt;
+        // Convert string to Int
+        bool IsuserScoreParsedIntoInt = Int32.TryParse(userScore, out userScoreInt);
+
+        string letter = "";
+        string gradeSign = "";
+
+        
+        // Test user score and assign the proper grade.
+        if ( userScoreInt >= 90 ) 
+        {
+            letter = "A";
+            // Console.WriteLine($"Your grade is A");
+
+        } 
+        else if ( userScoreInt >= 80 )
+        {
+            letter = "B";
+            // Console.WriteLine($"Your grade is B");
+
+        }
+        else if ( userScoreInt >= 70 )
+        {
+            letter = "C";
+            // Console.WriteLine($"Your grade is C");
+
+        } 
+        else if ( userScoreInt >= 60 )
+        {
+            letter = "D";
+            // Console.WriteLine($"Your grade is D");
+
+        }
+        else if ( userScoreInt < 60 )
+        {
+            letter = "F";
+            // Console.WriteLine($"Your grade is F");
+        }
+
+
+        // return a message
+        string message;
+        if (userScoreInt >= 70)
+        {
+            message = "Congratulations, you passed this class!";
+            // Console.WriteLine("Congratulations, you passed this class!");
+        } else 
+        {
+            message = "Ew! You can do better than this next time.";
+            // Console.WriteLine("Ew! You can do better than this next time.");
+        }
+
+        // Get the last digit of the user score by MODULUS division and return {sign}
+        int lastDigitOfUserScoreInt = userScoreInt % 10;
+        
+        if (lastDigitOfUserScoreInt >= 7)
+        {
+            gradeSign = "+";
+        } else if (lastDigitOfUserScoreInt <= 3)
+        {
+            gradeSign = "-";
+        }else {
+            gradeSign = "";
+        }
+
+        // Display overall message to the user
+        Console.WriteLine($"Your grade is {letter}{gradeSign}");
+        Console.WriteLine(message);
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,30 +5,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Console.WriteLine("Hello Prep4 World!");
-
-        // // To create a list, 
-
-        // List<int> numbers = new List<int>();
-        // List<string> strings = new List<string>();
-
-        // numbers.Add(1);
-        // numbers.Add(3);
-        // numbers.Add(10);
-
-        // strings.Add("John");
-        // strings.Add("Peace");
-        // strings.Add("Christopher");
-
-        // Console.WriteLine($"{numbers[2]}, {strings[2]}");
-
-        // Console.WriteLine($"{strings.Count}");
-
-        // foreach (string word in strings)
-        // {
-        //     Console.WriteLine(word);
-        // }
-
         List<int> ListOfNumbers = new List<int>();
 
         // Set default userInput to allow loop to run
@@ -41,20 +17,17 @@ class Program
             Console.Write("Enter number: ");
             userInput = int.Parse(Console.ReadLine());
 
-
             // Add number to the list if user inserted digits is not equal 0
             if (userInput != 0)
             {
                 ListOfNumbers.Add(userInput);
             }
-
         } while (userInput != 0);
 
 
         int sum = 0;
         int maxNum = -1;
-        int smallPositive = 1000000;
-
+        int smallPositive = 999999999;
 
         foreach (int number in ListOfNumbers)
         {
@@ -72,12 +45,10 @@ class Program
             {
                 smallPositive = number;
             }
-
-
         }
 
 
-        decimal count = ListOfNumbers.Count();
+        // decimal count = ListOfNumbers.Count();
         // Console.WriteLine(count);
         // decimal avgNum = 0M;
         // avgNum = (sum / ListOfNumbers.Count);
@@ -89,8 +60,6 @@ class Program
         Console.WriteLine($"The average is: {avgNum}");
         Console.WriteLine($"The largest number is: {maxNum}");
         Console.WriteLine($"The smallest positive number is: {smallPositive}");
-        // Console
-
 
         // To sort the data
         ListOfNumbers.Sort();
