4aac9f937d99017ea1a6d8a64d211855eb2f1c64
John Adebayo <johnayomide31@gmail.com>
2024-12-18 19:47:57 +0100

    exercise project done.

---
+++ foundation/Foundation3/Activity.cs
@@ -0,0 +1,43 @@
+using System;
+
+// Base class
+public abstract class Activity
+{
+    private string _date;
+    private int _minutes;
+
+    public Activity(string date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+    public string GetDate()
+    {
+        return _date;
+    }
+
+    public void SetDate(string date)
+    {
+        _date = date;
+    }
+
+    public int GetMinutes()
+    {
+        return _minutes;
+    }
+
+    public void SetMinutes(int minutes)
+    {
+        _minutes = minutes;
+    }
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+
+    public string GetSummary()
+    {
+        return $"{GetDate():dd MMM yyyy} {GetType().Name} ({GetMinutes()} min): Distance {GetDistance():F2} miles, Speed {GetSpeed():F2} mph, Pace {GetPace():F2} min per mile";
+    }
+}
